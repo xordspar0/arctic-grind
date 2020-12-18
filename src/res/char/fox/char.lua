450e9f93649fa6e03285c20f02f786e3ef62e9c5
Jordan Christiansen <xordspar0@gmail.com>
2020-12-17 23:57:54 -0600

    Clean up char.lua
    First I removed an unnecessary table: {sprite.new()} can just be
    sprite.new().
    
    Second, I reflowed all the whitespace in the file. With the new table
    structure, this was needed.

--- src/res/char/fox/char.lua
+++ src/res/char/fox/char.lua
@@ -5,57 +5,49 @@ local Fox = {}
 Fox.__index = Fox
 
 Fox.animations = {
-	jump = {
-		sprite = sprite.new(
-			resources.loadSprite("fox", "fox.png"),
-			{						-- List of all of frames in the animation.
-				{x = 0, y = 0},
-			},
-			64, 64, 0,				-- The width and height of each frame, how high the feet are in that frame.
-			0,						-- The frames per second of this animation.
-			false                   -- Whether this animation loops.
-		)
-	},
+	jump = sprite.new(
+		resources.loadSprite("fox", "fox.png"),
+		{                       -- List of all of frames in the animation.
+			{x = 0, y = 0},
+		},
+		64, 64, 0,              -- The width and height of each frame, how high the feet are in that frame.
+		0,                      -- The frames per second of this animation.
+		false                   -- Whether this animation loops.
+	),
 
-	stand = {
-		sprite = sprite.new(
-			resources.loadSprite("fox", "fox.png"),
-			{
-				{x = 0, y = 0},
-			},
-			64, 64, 0,
-			0,
-			false
-		)
-	},
+	stand = sprite.new(
+		resources.loadSprite("fox", "fox.png"),
+		{
+			{x = 0, y = 0},
+		},
+		64, 64, 0,
+		0,
+		false
+	),
 
-	walk = {
-		sprite = sprite.new(
-			resources.loadSprite("fox", "fox.png"),
-			{
-				{x = 64, y = 0},
-				{x = 128, y = 0},
-				{x = 192, y = 0},
-				{x = 0, y = 0},
-			},
-			64, 64, 0,
-			5,
-			true
-		)
-	},
+	walk = sprite.new(
+		resources.loadSprite("fox", "fox.png"),
+		{
+			{x = 64, y = 0},
+			{x = 128, y = 0},
+			{x = 192, y = 0},
+			{x = 0, y = 0},
+		},
+		64, 64, 0,
+		5,
+		true
+	),
 
-	scrunched = {
-		sprite = sprite.new(
-			resources.loadSprite("fox", "fox.png"),
-			{
-				{x = 0, y = 64},
-				{x = 64, y = 64},
-			},
-			64, 64, 0,
-			10,
-			false
-		)
-	},
+	scrunched = sprite.new(
+		resources.loadSprite("fox", "fox.png"),
+		{
+			{x = 0, y = 64},
+			{x = 64, y = 64},
+		},
+		64, 64, 0,
+		10,
+		false
+	),
 }
 
 Fox.currentAnim = Fox.animations.stand
@@ -70,21 +62,21 @@ function Fox:setAnim(animation)
 	self.currentAnim = self.animations[animation]
 	self.currentAnimName = animation
 	self.animationOver = false
-	self.currentAnim.sprite:resetFrame()
+	self.currentAnim:resetFrame()
 end
 
 function Fox:update(dt)
-	self.currentAnim.sprite:update()
+	self.currentAnim:update()
 
 	-- If this is the last frame of the animation, check to see if we should do
 	-- anything.
-	if self.currentAnim.sprite:atLastFrame() and self.currentAnim.loop == false then
+	if self.currentAnim:atLastFrame() and self.currentAnim.loop == false then
 		self.animationOver = true
 	end
 end
 
 function Fox:draw(x, y, facing)
-	self.currentAnim.sprite:draw(x, y, facing)
+	self.currentAnim:draw(x, y, facing)
 end
 
 return setmetatable({}, Fox)
